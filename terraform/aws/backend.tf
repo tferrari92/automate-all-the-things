28f7c69fd285b923605d96eefee137de2f572ae4
Tomas Ferrari <tferrari.92@gmail.com>
2023-05-06 16:27:55 -0300

    dsdfs

--- terraform/aws/backend.tf
+++ terraform/aws/backend.tf
@@ -8,3 +8,11 @@ terraform {
     encrypt        = true
   }
 }
+provider "aws" {
+  region = var.region
+
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+
+  # other options for authentication
+}
