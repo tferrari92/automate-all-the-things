0c62f91d81bfbac98f34ad9de1b9dcc59b13ef5a
Tomas Ferrari <tferrari.92@gmail.com>
2023-05-06 16:16:48 -0300

    dsdfs

--- terraform/aws/backend.tf
+++ terraform/aws/backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     bucket         = "aatt-tf-state"
     dynamodb_table = "terraform-state-locking"
-    key            = "aa-tt.tfstate"
+    key            = "terraform.tfstate"
     region         = "us-east-2"
     encrypt        = true
   }
