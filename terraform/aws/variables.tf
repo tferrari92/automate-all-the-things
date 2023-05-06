06826094b5d01760fef276d5d54379c9864dcca4
Tomas Ferrari <tferrari.92@gmail.com>
2023-05-06 15:58:26 -0300

    dsdfs

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -12,7 +12,6 @@ variable "aws_secret_key" {
 variable "region" {
   description = "The aws region. https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-regions-availability-zones.html"
   type        = string
-  default     = "us-east-1"
 }
 
 variable "availability_zones_count" {
