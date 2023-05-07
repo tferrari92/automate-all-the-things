a18fec2d87a0e72aae63667957fd5d8a7b53d01b
Tomas Ferrari <tferrari.92@gmail.com>
2023-05-07 18:46:08 -0300

    hola

--- terraform/aws/module.tf
+++ terraform/aws/module.tf
@@ -3,4 +3,8 @@ module "eks-kubeconfig" {
   version = "2.0.0"
   # insert the 1 required variable here
   cluster_name = aws_eks_cluster.this.name
+
+  depends_on = [
+    aws_eks_cluster.this
+  ]
 }
