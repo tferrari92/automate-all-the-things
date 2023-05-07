548b7450a64479e61a8f758c68fa2f16bc342894
Tomas Ferrari <tferrari.92@gmail.com>
2023-05-07 20:09:48 -0300

    hola

--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -9,3 +9,8 @@ output "cluster_endpoint" {
 output "cluster_ca_certificate" {
   value = aws_eks_cluster.this.certificate_authority[0].data
 }
+
+output "kubeconfig" {
+  description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
+  value       = module.eks-kubeconfig.kubeconfig
+}
