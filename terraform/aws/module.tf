8cf710c83cbe6fe50a7c1cee300aafca98be1429
Tomas Ferrari <tferrari.92@gmail.com>
2023-05-07 19:27:28 -0300

    hola

--- terraform/aws/module.tf
+++ terraform/aws/module.tf
@@ -2,7 +2,7 @@ module "eks-kubeconfig" {
   source  = "hyperbadger/eks-kubeconfig/aws"
   version = "2.0.0"
   # insert the 1 required variable here
-  cluster_name = aws_eks_cluster.this.name
+  cluster_name = "automate-all-the-things-cluster"
 
   depends_on = [
     aws_eks_cluster.this
