535670bbda6095f0a818f74aea325b0240481697
Tomas Ferrari <tferrari.92@gmail.com>
2023-05-11 15:31:36 -0300

    hola

--- python/local/00-initial-setup.py
+++ python/local/00-initial-setup.py
@@ -93,35 +93,37 @@ data = {
     "AATT_AWS_SECRET_ACCESS_KEY": aws_secret_access_key
 }
 
-# def replace_keys_in_file(file_path, replacements):
-#     with open(file_path, 'r') as file:
-#         content = file.read()
-    
-#     for key, value in replacements.items():
-#         content = content.replace(key, value)
-    
-#     with open(file_path, 'w') as file:
-#         file.write(content)
+# Save the data dictionary to a JSON file
+output_file = root_dir_path + "/info.json"
+with open(output_file, "w") as file:
+    json.dump(data, file, indent=4)
 
 
-# def search_and_replace(directory, replacements):
-#     for root, _, files in os.walk(directory):
-#         for file_name in files:
-#             if file_name == 'info.json':
-#                 continue  # Skip processing info.json file
-#             file_path = os.path.join(root, file_name)
-#             replace_keys_in_file(file_path, replacements)
+
+def replace_keys_in_file(file_path, replacements):
+    with open(file_path, 'r') as file:
+        content = file.read()
+    
+    for key, value in replacements.items():
+        content = content.replace(key, value)
+    
+    with open(file_path, 'w') as file:
+        file.write(content)
+
+
+def search_and_replace(directory, replacements):
+    for root, _, files in os.walk(directory):
+        for file_name in files:
+            if file_name == 'info.json':
+                continue  # Skip processing info.json file
+            file_path = os.path.join(root, file_name)
+            replace_keys_in_file(file_path, replacements)
+
 
 def main():
-    # Create a dictionary with the variable names and their values
-    data = {
-        "AATT_FULL_NAME": full_name,
-        "AATT_APP_NAME": app_name,
-        "AATT_AZ_DEVOPS_ORG": az_devops_org,
-        "AATT_DOCKERHUB_USERNAME": dockerhub_username,
-        "AATT_AWS_ACCESS_KEY": aws_access_key,
-        "AATT_AWS_SECRET_ACCESS_KEY": aws_secret_access_key
-    }
+    # Read the JSON file
+    with open('info.json', 'r') as json_file:
+        data = json.load(json_file)
 
     # Extract the replacements
     replacements = {key: str(value) for key, value in data.items()}
@@ -129,32 +131,15 @@ def main():
     # Specify the directory to search
     directory = root_dir_path  
 
-    for root, _, files in os.walk(directory):
-        for file_name in files:
-            if file_name == 'info.json':
-                continue  # Skip processing info.json file
-            file_path = os.path.join(root, file_name)
-            with open(file_path, 'r') as file:
-                content = file.read()
-            
-            for key, value in replacements.items():
-                content = content.replace(key, value)
-            
-            with open(file_path, 'w') as file:
-                file.write(content)
-            # replace_keys_in_file(file_path, replacements)
-
     # Search and replace keys in files
-    # search_and_replace(directory, replacements)
+    search_and_replace(directory, replacements)
 
-    # print('Replacement completed successfully!')
+    print('Replacement completed successfully!')
 
-# Save the data dictionary to a JSON file
-# output_file = root_dir_path + "/info.json"
-# with open(output_file, "w") as file:
-#     json.dump(data, file, indent=4)
 
-print_gradually("That's it! All files have been customized with your info.\nDon't worry, this file is specified in the .gitignore so it won't be pushed if you decide to upload this.\n\n")
+main()
+
+print_gradually("That's it! All the info you provided has been saved in an 'info.json' file on the repo root directory.\nDon't worry, this file is specified in the .gitignore so it won't be pushed if you decide to upload this.\nIf you need to modify any of this info, you can just edit the info.json file.\n\n")
 
 print_gradually_fast(" _   _    _    ____  ______   __     _   _   _ _____ ___  __  __    _  _____ ___ _   _  ____ _ _ _ \n| | | |  / \  |  _ \|  _ \ \ / /    / \ | | | |_   _/ _ \|  \/  |  / \|_   _|_ _| \ | |/ ___| | | |\n| |_| | / _ \ | |_) | |_) \ V /    / _ \| | | | | || | | | |\/| | / _ \ | |  | ||  \| | |  _| | | |\n|  _  |/ ___ \|  __/|  __/ | |    / ___ \ |_| | | || |_| | |  | |/ ___ \| |  | || |\  | |_| |_|_|_|\n|_| |_/_/   \_\_|   |_|    |_|   /_/   \_\___/  |_| \___/|_|  |_/_/   \_\_| |___|_| \_|\____(_|_|_)")
 
