0ec54187c1a47a12b2c7be6008f48ba69ed1d632
Tomas Ferrari <tomas.ferrari@sendati.com>
2023-05-12 15:44:53 -0300

    hola

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -3,20 +3,20 @@ variable "project" {
   type = string
 }
 
-variable "region" {
-  description = "The aws region. https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-regions-availability-zones.html"
-  type        = string
-}
+# variable "region" {
+#   description = "The aws region. https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-regions-availability-zones.html"
+#   type        = string
+# }
 
-variable "aws_access_key" {
-  description = "AWS access key"
-  type        = string
-}
+# variable "aws_access_key" {
+#   description = "AWS access key"
+#   type        = string
+# }
 
-variable "aws_secret_key" {
-  description = "AWS secret key"
-  type        = string
-}
+# variable "aws_secret_key" {
+#   description = "AWS secret key"
+#   type        = string
+# }
 
 variable "availability_zones_count" {
   description = "The number of AZs."
--- terraform/backend/provider.tf
+++ terraform/backend/provider.tf
@@ -9,8 +9,8 @@ terraform {
 }
 
 provider "aws" {
-  region = var.region
+  # region = var.region
 
-  # access_key = ""
-  # secret_key = ""
+  # access_key = var.aws_access_key
+  # secret_key = var.aws_secret_key
 }
