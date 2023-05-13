a3eafd994c7aaf73a647f1b208c85f833deaaccd
Tomas Ferrari <tomas.ferrari@sendati.com>
2023-05-12 21:32:33 -0300

    hola

--- terraform/aws/module.tf
+++ terraform/aws/module.tf
@@ -1,7 +1,7 @@
 module "eks-kubeconfig" {
   source  = "hyperbadger/eks-kubeconfig/aws"
   version = "2.0.0"
-  # insert the 1 required variable here
+
   cluster_name = "${var.project}-cluster"
 
   depends_on = [
