c35c3d38b938755681823ff96561e65afa253447
tferrari92 <tferrari.92@gmail.com>
2023-05-14 14:09:50 -0300

    hola

--- terraform/aws/provider.tf
+++ terraform/aws/provider.tf
@@ -8,7 +8,17 @@ terraform {
       version = "4.27.0"
     }
   }
+  
+  backend "s3" {
+    bucket         = "cafe-tf-state-bucket" # This value was modified by the initial-setup python script
+    dynamodb_table = "cafe-tf-state-dynamo-db-table" # This value was modified by the initial-setup python script
+    key            = "terraform.tfstate"
+    region         = "us-east-2"
+    encrypt        = true
+  }
 }
+
+
 
 provider "aws" {
   # region = var.region
