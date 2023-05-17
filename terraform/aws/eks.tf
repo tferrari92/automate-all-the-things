cc607ec09e8c025135ba811626ee1036680bd5ef
tferrari92 <tferrari.92@gmail.com>
2023-05-17 11:09:56 -0300

    hola

--- terraform/aws/eks.tf
+++ terraform/aws/eks.tf
@@ -85,7 +85,7 @@ resource "aws_iam_role_policy_attachment" "amazon-ec2-container-registry-read-on
 
 resource "aws_eks_node_group" "private-nodes" {
   cluster_name    = aws_eks_cluster.cluster.name
-  version         = var.cluster_version
+  version         = "1.22"
   node_group_name = "private-nodes"
   node_role_arn   = aws_iam_role.nodes.arn
 
