5cdb54a9db8bb3bcf5adbd732aa5a18e9910a3f7
tferrari92 <tferrari.92@gmail.com>
2023-05-17 14:29:03 -0300

    hola

--- terraform/aws/network.tf
+++ terraform/aws/network.tf
@@ -34,7 +34,7 @@ resource "aws_subnet" "private-us-east-2a" {
   tags = {
     "Name"                                      = "private-us-east-2a"
     "kubernetes.io/role/internal-elb"           = "1"
-    "kubernetes.io/cluster/${var.project}" = "owned"
+    "kubernetes.io/cluster/${var.project}-cluster" = "owned"
   }
 }
 
@@ -46,7 +46,7 @@ resource "aws_subnet" "private-us-east-2b" {
   tags = {
     "Name"                                      = "private-us-east-2b"
     "kubernetes.io/role/internal-elb"           = "1"
-    "kubernetes.io/cluster/${var.project}" = "owned"
+    "kubernetes.io/cluster/${var.project}-cluster" = "owned"
   }
 }
 
@@ -59,7 +59,7 @@ resource "aws_subnet" "public-us-east-2a" {
   tags = {
     "Name"                                      = "public-us-east-2a"
     "kubernetes.io/role/elb"                    = "1"
-    "kubernetes.io/cluster/${var.project}" = "owned"
+    "kubernetes.io/cluster/${var.project}-cluster" = "owned"
   }
 }
 
@@ -72,7 +72,7 @@ resource "aws_subnet" "public-us-east-2b" {
   tags = {
     "Name"                                      = "public-us-east-2b"
     "kubernetes.io/role/elb"                    = "1"
-    "kubernetes.io/cluster/${var.project}" = "owned"
+    "kubernetes.io/cluster/${var.project}-cluster" = "owned"
   }
 }
 
