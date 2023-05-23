2ae8cacd2fbcf7a622f0ecd86c0a77fa29d87629
Tomas Ferrari <90274987+tferrari92@users.noreply.github.com>
2023-05-23 15:21:42 -0300

    Update initial-setup.py

--- python/initial-setup.py
+++ python/initial-setup.py
@@ -1,8 +1,5 @@
-# import sys
 import time
 import os
-# import json
-import getpass
 
 
 print('''                                                                                             \n                                                                         ▄                          \n      ╒█▄    ,¬▄█AA ▄▄▄ ¬█▄█▄ ██`▀▀█▀¬██▀▀     ▄▄  ▄    ▄      a▄N█▀▀▀█  █ █▀▀▀▀▀                   \n      █"█▄█═ █▌ █  ╒█ -█µ█▀▀█µ███  █. █&       ███▄▐▌   █         █   █▀╘█ ▐▌-                      \n      █▄███▌ █▌ ▀█ ▐█▄▄█"█  ▐▌█-▀ⁿ ▐⌐▐█▄;      ▀ ▀┐▐███,███▀"     █   ▀    ▐█A⌐                     \n      █⌐ └█▀██   ▀ⁿ  ¬¬     `▀└        `     ▀▀▀▀▀▀└`└└└└└`▀▀▀                                      \n                 ▄▄▄▄∩▌ j▌ ╓█  ╒▄,  ▄ ╒█    ▄Æ▄  ╒r                                 ,╓              \n              '▀▀█    █44█ ]█╖ ▐▌▀█ █ ▐▌ ▄▄ ▀▀R▄ █                    ▀█        ╓@╢▒╜               \n                 █    █  ▀  █▒▒▒▌  ▀█ ╘█,█▀▄▄▄▄▀ ▀   ╖               , ▌    ╓╥╢▒▒▒╢`                \n                 '           ╢▒▒▒▒@╖    ¬¬ ▀╢░   e  ╢▒           ╓╢╢╜` ▌,╥╢▒▒▒▒▒▒╜                  \n                             ╙▒▒▒▒▒▒╢╗╖    ║▒▒╖    ║▒▒▒       ╓╢╢▒▒╜,╓▐▒▒▒▒▒▒▒▒╢                    \n                              ║▒▒▒▒▒▒▒▒╢@╖ ║▒▒▒╢╖ j▒▒▒╢    ╓@╢▒▒▒▒▒╢▒▒█▒▒▒▒▒▒▒╜                     \n                               ╢▒▒▒▒▒▒▒▒▒▒╢╢▒▒▒▒▒▒▒▒▒▒▒░╓╢╢▒▒▒▒▒▒▒▒▒▒▒▌▒▒▒▒▒╣                       \n                  .╓           ╙▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▀⌠!░░░░▀█▒▒▒▒▒█▀'╙Ñ▒▒▒▒▌▒▒▒▒╜                        \n                    ╙╢╢╗╖       ╙▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒W▄W╧²"╙▀'¬¬▐▌  ` ▒▒▒▒▒▒▒▒╜                         \n                     ▄▄▓@▒▒p╖    ╙▒▒▒▒▒▒▒▒▒▒▒▒▒╩█,     ▄∞M══▄▀▀∞²└▀▒▒▐▒▒▒░                          \n                  ,█╢╣▓▓▓╢█▒▒█▒@╖╖║▒▒▒▒▒▒▒▒▒▒▌⌐  ▐,≈M▀▄▄██████     ╙▒█▒▒░                           \n                 ╒╣▓▓▓▓▀▀█▒╢╢╢▒▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒╗▄∞█▀▄███████████     █▌▒▒                 ,,╓╥╢╜     \n           ▄▄▄▄▄,██▀██     █▒╢╢█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█ "▀████████████     █▒▒╓╖╥@╢╢╢╜     ,╓@╢╢▒║╜`       \n        ▄█▒╢╣╣█▀▒▒▒▒▒█▄, ╙▒▒████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄   ▀██████████▌    ▓▒▒▒▒▒▒╢`  ,╓╥╢╢▒▒▒╢╜`          \n       ▐▒╣╣╢▓╩▓▀▄▒▒▒▒▒█▀▒▒▒╢╢▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█    ████████▀L   █╢█▒▒▒╢▒╓╥╢╢▒▒▒▒▒╢╜              \n       ██▄██    ▐▒▒▄▒▄█▒╢╢╢▒█▀╙╢▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█   ██▀███▀▄▀   █╢╢╢▌▒▒▒▒▒▒▒▒▒▒╢╜                 \n       ▌▒▒╢█    ▐▒▒▒██▓▓█░▒╖╥@@╢▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▄   "▀▀└`    █▒║╢╢▓▒▒▒▒▒▒▒▒╨`                   \n       ▀▒╢╢╢▒█▓▒▒▒▒▒█╙▀▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█╦▄▄▄▄▄▄▄@▓▒╢╢╢╢╢╢█▒▒▒▒▒╜`                     \n        └▀╣▒▄█▒▒▒▒█▀╖╖╓,▀▓▓█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒╢╢║╢║║╢╢╢╢╢╢╢╢╢╢╫▒▒▒▒╢╢╢╢╢╢╢╢╢╢╢@@@╗╥╖╓,     \n            -└▀▀╙▒▒▒▒▒▒▒▒▒▀▓▓█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒╢╢╢╢╢╢╢╢╢╢╢╢╢╢╢╢█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒╢Ñ    \n                 └╢▒▒▒▒▒▒▒▒▒█▓██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█╣╢╢╢╢╢╢╢╢╢╢╢╢╢╢╢╫▒▒▒▒▒▒▒▒▒▒▒▒▒▒╢╜╜`      \n''')
@@ -12,18 +9,19 @@ print('''                                                                       
 script_path = os.path.abspath(__file__)
 root_dir_path = os.path.dirname(os.path.dirname(os.path.dirname(script_path)))
 
-# print(root_dir_path)
 
-# Function to print text gradually
+# Functions to print text gradually
 def print_gradually(text):
     for char in text:
         print(char, end='', flush=True)
-        time.sleep(0.01)  
+        time.sleep(0.03)
+
 
 def print_gradually_fast(text):
     for char in text:
         print(char, end='', flush=True)
-        time.sleep(0.005) 
+        time.sleep(0.005)
+
 
 # Prompt the user for their full name character by character
 print_gradually("Please enter your full name: ")
@@ -33,30 +31,27 @@ full_name = input()
 first_name = full_name.split()[0]
 
 
-# Prompt the user for their favorite food, addressing them by their first name
-# print_gradually("Thank you, " + first_name + "! Welcome to Automate All The Things! Are you exited?: ")
-# favorite_food = input()
-
-
 def prompt_with_options(message, options):
     # Print the message and options
     print_gradually(message + "\n")
     for i, option in enumerate(options):
         print(f"{i+1}. {option}")
-    
+
     # Keep asking for input until a valid option is chosen
     while True:
         print_gradually("Enter your choice: ")
         choice = input()
-        
+
         # Validate the input
         if choice.isdigit() and 1 <= int(choice) <= len(options):
             return options[int(choice)-1]
-        
+
         print("Invalid input. Please enter the number corresponding to your choice.")
 
+
 # Define the message and options
-message = "Thank you, " + first_name + "! Welcome to Automate All The Things! Are you exited?"
+message = "Thank you, " + first_name + \
+    "! Welcome to Automate All The Things! Are you exited?"
 options = ['Yes', 'YEEEAAAAHHHHHH!!!!!']
 
 # Prompt the user and get their choice
@@ -69,25 +64,31 @@ else:
     print_gradually("WOOOOHOOOOOOOO!!!! DAMN RIGHT!!\n")
 
 
-print_gradually("Alright, let's get the necessary details. What will be the name of your app?: ")
-app_name = input()
+# Function to check that input is only alphanumeric + "-" + "_"
+def check_if_valid(input_string):
+    while not input_string.replace("-", "").replace("_", "").isalnum():
+        print("This value can only contain alphanumeric characters, hyphens(-) and underscores(_). Please provide a valid input:")
+        input_string = input()
+    return input_string
 
-print_gradually("Great name! What's the name of your Azure DevOps Organization?: ")
+
+print_gradually(
+    "Alright, let's get the necessary details. What will be the name of your app?: ")
+app_name = check_if_valid(input())
+
+print_gradually(
+    "Great name! What's the name of your Azure DevOps Organization?: ")
 az_devops_org = input()
 
 print_gradually("Nice! Sounds professional! What's your GitHub username?: ")
 github_username = input()
 
-print_gradually("In what AWS region will you be deploying your resources? (e.g. 'us-east-2'): ")
+print_gradually(
+    "In what AWS region will you be deploying your resources? (e.g. 'us-east-2'): ")
 aws_region = input()
 
 print_gradually("Ok. What's your DockerHub username?: ")
 dockerhub_username = input()
-
-# print_gradually("I'm gonna need you to get me your AWS Access Keys:\n")
-# aws_access_key = getpass.getpass("- Access Key: ")
-# aws_secret_access_key = getpass.getpass("- Secret Access Key: ")
-
 
 
 # Create a dictionary with the variable names and their values
@@ -98,49 +99,40 @@ data = {
     "AATT_GITHUB_USERNAME": github_username,
     "AATT_AWS_REGION": aws_region,
     "AATT_DOCKERHUB_USERNAME": dockerhub_username,
-    # "AATT_AWS_ACCESS_KEY": aws_access_key,
-    # "AATT_AWS_SECRET_ACCESS_KEY": aws_secret_access_key
 }
 
-# Save the data dictionary to a JSON file
-# output_file = root_dir_path + "/info.json"
-# with open(output_file, "w") as file:
-#     json.dump(data, file, indent=4)
 
 def replace_keys_in_file(file_path, replacements):
     with open(file_path, 'r') as file:
         content = file.read()
-    
+
     for key, value in replacements.items():
         content = content.replace(key, value)
-    
+
     with open(file_path, 'w') as file:
         file.write(content)
+
 
 def search_and_replace(directory, replacements):
     for root, _, files in os.walk(directory):
         for file_name in files:
             file_path = os.path.join(root, file_name)
             if file_name in ['application.yaml',
+                             '00-deploy-infra.yml',
+                             '01-deploy-argocd.yml',
+                             '02-build-and-deploy-app.yml',
+                             '03-destroy-everything.yml',
                              'values.yaml',
-                             'terraform.tfvars',
-                             '00-deploy-backend.yml',
-                             '01-deploy-eks.yml',
-                             '02-deploy-argocd.yml',
-                             'index.html']:
+                             'terraform.tfvars']:
                 replace_keys_in_file(file_path, replacements)
 
 
 def main():
-    # Read the JSON file
-    # with open('info.json', 'r') as json_file:
-    #     data = json.load(json_file)
-
     # Extract the replacements
     replacements = {key: str(value) for key, value in data.items()}
 
     # Specify the directory to search
-    directory = root_dir_path  
+    directory = root_dir_path
 
     # Search and replace keys in files
     search_and_replace(directory, replacements)
@@ -149,18 +141,7 @@ def main():
 main()
 
 
-print_gradually("That's it! All necessary files were updated with the info you provided.\nYou can go back to the README and carry on with the guide.\n\n")
-# print_gradually("That's it! All necessary files were updated with the info you provided. The info has also been saved in an 'info.json' file on the repo root directory.\nDon't worry, this file is specified in the .gitignore so it won't be pushed if you decide to upload this.\nIf you need to modify any of this info, you can just edit the info.json file.\n\n")
-# print_gradually("That's it! The info you provided has been saved in an 'info.json' file on the repo root directory and all necessary files have been updated.\nDon't worry, this file is specified in the .gitignore so it won't be pushed if you decide to upload this.\nYou can go back to the README on the repo and carry on with the guide.\n\n")
-# print_gradually("That's it! All the info you provided has been saved in an 'info.json' file on the repo root directory.\nDon't worry, this file is specified in the .gitignore so it won't be pushed if you decide to upload this.\nYou can go back to the README on the repo and carry on with the guide.\n\n")
-# print_gradually("That's it! All the info you provided has been saved in an 'info.json' file on the repo root directory.\nDon't worry, this file is specified in the .gitignore so it won't be pushed if you decide to upload this.\nIf you need to modify any of this info, you can just edit the info.json file.\n\n")
-# print_gradually("That's it! The info you provided has been saved in an 'info.json' file on the repo root directory and all necessary files have been updated.\nDon't worry, this file is specified in the .gitignore so it won't be pushed if you decide to upload this.\nIf you need to modify any of this info, you can just edit the info.json file.\n\n")
+print_gradually(
+    "That's it! All necessary files were updated with the info you provided.\nYou can go back to the README and carry on with the guide.\n\n")
 
 print_gradually_fast(" _   _    _    ____  ______   __     _   _   _ _____ ___  __  __    _  _____ ___ _   _  ____ _ _ _ \n| | | |  / \  |  _ \|  _ \ \ / /    / \ | | | |_   _/ _ \|  \/  |  / \|_   _|_ _| \ | |/ ___| | | |\n| |_| | / _ \ | |_) | |_) \ V /    / _ \| | | | | || | | | |\/| | / _ \ | |  | ||  \| | |  _| | | |\n|  _  |/ ___ \|  __/|  __/ | |    / ___ \ |_| | | || |_| | |  | |/ ___ \| |  | || |\  | |_| |_|_|_|\n|_| |_/_/   \_\_|   |_|    |_|   /_/   \_\___/  |_| \___/|_|  |_/_/   \_\_| |___|_| \_|\____(_|_|_)\n\n")
-
-
-# Print the entered name and the script's absolute path
-# print("\nYour full name is:", full_name)
-# print("\nYour app name is:", app_name)
-# print("Rootdir  path:", root_dir_path)
-# print("Variables saved in:", output_file)
