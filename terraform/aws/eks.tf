fdc584599b43b18b9633c01126ca3bbb97b865ec
Tomas Ferrari <90274987+tferrari92@users.noreply.github.com>
2023-06-27 18:36:39 -0300

    Update eks.tf

--- terraform/aws/eks.tf
+++ terraform/aws/eks.tf
@@ -98,7 +98,7 @@ resource "aws_eks_node_group" "private-nodes" {
   instance_types = ["t3.small"]
 
   scaling_config {
-    desired_size = 2
+    desired_size = 3
     max_size     = 5
     min_size     = 0
   }
