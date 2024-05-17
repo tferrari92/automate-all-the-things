7092c1102a34a4bf373535d745a9c1a94c76d858
Tomas Ferrari <90274987+tferrari92@users.noreply.github.com>
2024-05-17 11:25:56 +0200

    Update security-groups.tf

--- terraform/aws/security-groups.tf
+++ terraform/aws/security-groups.tf
@@ -19,6 +19,7 @@ resource "aws_security_group" "default" {
 
 resource "aws_security_group" "databases" {
   name_prefix = var.project
+  description = "Security group for databases"
   vpc_id      = aws_vpc.main.id
 
   # Allow the EC2 located in public-subnet-c to connect only through port 22 (SSH)
@@ -29,11 +30,19 @@ resource "aws_security_group" "databases" {
     cidr_blocks = [aws_subnet.public-subnet-c.cidr_block]
   }
 
-  # Allow the EKS nodes located in private-subnet-a and private-subnet-b to connect.
+  # Allow the EC2 located in public-subnet-c to connect with redis-cli
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = [aws_subnet.public-subnet-c.cidr_block, aws_subnet.private-subnet-a.cidr_block, aws_subnet.private-subnet-b.cidr_block]
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp" # All
+    cidr_blocks = [aws_subnet.public-subnet-c.cidr_block]
+  }
+
+  # Allow the EKS nodes located in private-subnet-a and private-subnet-b to connect to DB.
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp" # All
+    cidr_blocks = [aws_subnet.private-subnet-a.cidr_block, aws_subnet.private-subnet-b.cidr_block]
   }
 }
